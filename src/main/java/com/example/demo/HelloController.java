508253ae7fce1ce653f6eda4e664d5c604b73fb3
Malichenko-Maksym <s221680@student.uek.krakow.pl>
2023-03-11 18:23:56 +0100

    almost done

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,57 +1,20 @@
 package com.example.demo;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Arrays;
-
-
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ModelAttribute;
-
 import org.springframework.ui.Model;
+
 
 @Controller
 @RequestMapping("/")
 public class HelloController {
 
-	private double val1;
-	private double val2;
-	private double sum;
+    private double exchangeRate = WebRequest.getRateFrom("http://api.nbp.pl/api/exchangerates/rates/a/gbp/?format=json");
 
-
-	public HelloController() {
-		sum=0;
-	}
-
-
-	@GetMapping
-	public String mainPage(Model model) {
-		model.addAttribute("newInput", new Input());
-		model.addAttribute("summa", sum);
-		return "main";
-	}
-
-	@PostMapping("/add")
-	public String Exchange(@ModelAttribute Input input) {
-		val1=Double.parseDouble(input.getInputGBP());
-		val2=Double.parseDouble(input.getInputPLN());    
-		System.out.println(input);
-		sum=val1*WebRequest.getRateFrom("http://api.nbp.pl/api/exchangerates/rates/a/gbp/?format=json")+val2;
-		System.out.println(sum);
-
-		return "redirect:/";
-	}
-	
-	@GetMapping("/error")
-	public String handleError() {
-		return "main";
-	}
+    @GetMapping
+    public String mainPage(Model model) {
+        model.addAttribute("exchangeRate", exchangeRate);
+        return "main";
+    }
 }
